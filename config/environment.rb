c6d8514d886e9b77b2fd2085700bac0e66b68628
Damian Hamill <damianham@gmail.com>
2012-12-12 10:23:14 +0700

    fix load paths

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the application
 require File.expand_path('../application', __FILE__)
 
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.dirname(__FILE__) + "/lib")
+$LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__) + "/.."))
+$LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__) + "/..") + "/lib")
